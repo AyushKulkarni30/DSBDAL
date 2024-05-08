07b6db6fe5e87edcef9c38e4e23152b16d421385
AyushKulkarni <94945772+AyushKulkarni30@users.noreply.github.com>
2024-05-08 08:50:57 +0530

    Update exp6main.py

--- Experiment_6/exp6main.py
+++ Experiment_6/exp6main.py
@@ -36,3 +36,38 @@ print(iris.corr())
 plt.subplots(figsize = (8,8))
 sns.heatmap(iris.corr(), annot=True, fmt="f").set_title("Corelation of attributes (petal and sepal")
 plt.show()
+X=iris.iloc[:,0:4].values
+y=iris.iloc[:,4].values
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+accuracy_Naive Bayes: 1.000
+precision_Naive Bayes: 1.000
+recall_Naive Bayes: 1.000
+f1-score_Naive Bayes : 1.000
+#Metrics
+from sklearn.metrics import make_scorer, accuracy_score,precision_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score ,precision_score,recall_score,f1_score
+#Model Select
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+#Train and Test split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=0)
+gaussian = GaussianNB()
+gaussian.fit(X_train, y_train)
+Y_pred = gaussian.predict(X_test)
+accuracy_nb=round(accuracy_score(y_test,Y_pred)* 100, 2)
+acc_gaussian = round(gaussian.score(X_train, y_train) * 100, 2)
+cm = confusion_matrix(y_test, Y_pred)
+accuracy = accuracy_score(y_test,Y_pred)
+precision =precision_score(y_test, Y_pred,average='micro')
+recall = recall_score(y_test, Y_pred,average='micro')
+f1 = f1_score(y_test,Y_pred,average='micro')
+print('Confusion matrix for Naive Bayes\n',cm)
+print('accuracy_Naive Bayes: %.3f' %accuracy)
+print('precision_Naive Bayes: %.3f' %precision)
+print('recall_Naive Bayes: %.3f' %recall)
+print('f1-score_Naive Bayes : %.3f' %f1)
